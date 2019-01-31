c2f7d11f34e28786c0a3aca2ab45cf5bc5ac2e23
groiq <35578027+groiq@users.noreply.github.com>
2019-01-31 18:05:31 +0100

    exercise magic square forming: setup

--- hackerrank-com/hackerrank-com/Program.cs
+++ hackerrank-com/hackerrank-com/Program.cs
@@ -7,43 +7,47 @@ namespace hackerrank_com
     class Program
     {
 
-        static int getMoneySpent(int[] keyboards, int[] drives, int b)
+        static int formingMagicSquare(int[][] s)
         {
-            int result = -1;
+            int result = 0;
 
-            foreach (int k in keyboards)
+            foreach(int[] i in s)
             {
-                foreach (int d in drives)
+                foreach(int j in i)
                 {
-                    if ((k+d > result) && (k+d <= b)) { result = k + d; }
+                    Console.Write(j + ",");
                 }
+                Console.WriteLine();
             }
 
             return result;
-         
-        }
 
+        }
 
 
         static void Main(string[] args)
         {
 
-            /*
-             * 10,(3,1),(5,2,8) -> 9
-             * 5,(4),(5) -> -1
-             */
+            int[] q1a = { 5, 3, 4 };
+            int[] q1b = { 1, 5, 8 };
+            int[] q1c = { 6, 4, 2 };
+            //int[][] q1 = { { 5, 3, 4 }, { 1, 5, 8 }, { 6, 4, 2 } };
+            int[][] q1 = { q1a, q1b, q1c };
+
+            int[] q2a = { 4, 9, 2 };
+            int[] q2b = { 3, 5, 7 };
+            int[] q2c = { 8, 1, 5 };
+            int[][] q2 = { q2a, q2b, q2c };
+
+            int[] q3a = { 4, 8, 2 };
+            int[] q3b = { 4, 5, 7 };
+            int[] q3c = { 6, 1, 6 };
+            int[][] q3 = { q3a, q3b, q3c };
 
             
-            int[] keyboards = { 3, 1 };
-            int[] drives = { 5, 2, 8 };
-            Console.WriteLine("9: " + getMoneySpent(keyboards, drives, 10));
-            int[] k1 = { 4};
-            int[] d1 = { 5};
-            Console.WriteLine("-1: " + getMoneySpent(k1, d1, 5));
-
-            //Console.WriteLine(keyboards);
-            //foreach (int i in keyboards) { Console.Write(i); }
-
+            Console.WriteLine(formingMagicSquare(q1));
+            Console.WriteLine(formingMagicSquare(q2));
+            Console.WriteLine(formingMagicSquare(q3));
 
             Console.ReadKey();
         }
