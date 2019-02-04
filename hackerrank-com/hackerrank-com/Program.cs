bd9ae0d0e9e8b249d56f34e47a5edcdcbb9c1f1c
groiq <35578027+groiq@users.noreply.github.com>
2019-02-04 20:33:13 +0100

    set up some helper functions

--- hackerrank-com/hackerrank-com/Program.cs
+++ hackerrank-com/hackerrank-com/Program.cs
@@ -6,11 +6,38 @@ namespace hackerrank_com
 {
     class Program
     {
+        static int[] sums(int[][] s)
+        {
+            // return the sums in this order:
+            // line 1 - line 2 - line 3 - col 1 - col 2 - col 3 - 
+            // diag 1 - diag 2
+            int[] result = new int[8];
+            return result;
+        }
+
+        static void printMagicSqare(int[][] s)
+        {
+            // print the magic square like this:
+
+            /*            | 15
+             *    --------|---
+             *     8  3  4| 15
+             *     1  5  9| 15
+             *     6  7  2| 15
+             *    --------|---
+             *    15 15 15| 15
+             */
+        }
 
         static int formingMagicSquare(int[][] s)
         {
+            // https://www.hackerrank.com/challenges/magic-square-forming/problem
+
+
+
             int result = 0;
 
+            // check input
             foreach(int[] i in s)
             {
                 foreach(int j in i)
@@ -19,6 +46,20 @@ namespace hackerrank_com
                 }
                 Console.WriteLine();
             }
+
+            // todo: generate some test output with the squares and the sums - 
+            // best as an extra function
+
+
+            // some thoughts:
+            // find the single operation that brings us closest to our goal.
+
+            // for that, find the horizontal / vertical sums that are least off.
+            // also, if two sums are the same, take those.
+
+
+
+
 
             return result;
 
