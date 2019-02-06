cdd7546efbea50dbbc8feab655ebc8793fc590f8
groiq <35578027+groiq@users.noreply.github.com>
2019-02-06 17:18:32 +0100

    picking numbers: set up exercise

---
+++ hackerrank-com/03-picking-numbers/03-picking-numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿// workspace for exercises from https://www.hackerrank.com/
+
+using System;
+
+namespace hackerrank_com
+{
+    class Program
+    // https://www.hackerrank.com/challenges/picking-numbers
+    {
+
+        public static int pickingNumbers(List<int> a)
+        {
+            int result = 0;
+
+
+            return result;
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Output goes here...");
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ hackerrank-com/hackerrank-basic-template.txt
@@ -0,0 +1,21 @@
+﻿// workspace for exercises from https://www.hackerrank.com/
+
+using System;
+
+namespace hackerrank_com
+{
+    class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Output goes here...");
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
