31a2e5a383600167379c90ee5deb04f26cab6594
groiq <35578027+groiq@users.noreply.github.com>
2019-02-06 18:32:37 +0100

    first attempt

--- hackerrank-com/03-picking-numbers/03-picking-numbers/Program.cs
+++ hackerrank-com/03-picking-numbers/03-picking-numbers/Program.cs
@@ -15,10 +15,35 @@ namespace hackerrank_com
 
             // loop through input: count all numbers
             // save to dict
+            var counter = new Dictionary<int, int>();
+            foreach (var item in a)
+            {
+                if (counter.ContainsKey(item)) { counter[item] += 1; }
+                else { counter.Add(item, 1); }
+            }
 
+            foreach (var pair in counter)
+            {
+                Console.WriteLine(pair);
+            }
 
             // loop through dict
-
+            
+            foreach (var curKey in counter.Keys)
+            {
+                int nextKey = curKey + 1;
+                Console.WriteLine(curKey + " -> " + nextKey);
+                if (counter.ContainsKey(nextKey))
+                {
+                    int curPicking = counter[curKey] + counter[nextKey];
+                    Console.WriteLine(curKey + " -> " + counter[curKey] + ", " 
+                        + nextKey + " -> " + counter[nextKey] + " => " + curPicking);
+                    if (curPicking > result)
+                    {
+                        result = curPicking;
+                    }
+                }
+            }
 
             return result;
 
