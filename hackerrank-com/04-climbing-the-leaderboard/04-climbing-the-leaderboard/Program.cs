a986b7acc03535cd1b6ae99f7cb4d090dca3b7fe
groiq <35578027+groiq@users.noreply.github.com>
2019-02-07 10:30:45 +0100

    interim solution without dense ranking

--- hackerrank-com/04-climbing-the-leaderboard/04-climbing-the-leaderboard/Program.cs
+++ hackerrank-com/04-climbing-the-leaderboard/04-climbing-the-leaderboard/Program.cs
@@ -9,9 +9,27 @@ namespace hackerrank_com
     {
         static int[] climbingLeaderboard(int[] scores, int[] alice)
         {
+            pprint(scores);
+            pprint(alice);
             int[] result = new int[alice.Length];
+            int pos = alice.Length - 1;
 
+            for (int i = 0; i < scores.Length; i++)
+            {
+                Console.WriteLine("i: " + i + ", scores[i]: " + scores[i] + ", pos: " + pos + ", alice[pos]: " + alice[pos]);
+                if (alice[pos] >= scores[i])
+                {
+                    result[pos] = i + 1;
+                    pos -= 1;
+                }
+            }
+            //Console.WriteLine(scores[scores.Length-1]);
+            if (alice[0] < scores[scores.Length-1])
+            {
+                result[0] = scores.Length + 1;
+            }
 
+            //pprint(result);
             return result;
         }
 
@@ -27,14 +45,18 @@ namespace hackerrank_com
             //Console.WriteLine("Output goes here...");
             int[] s1 = { 100, 100, 50, 40, 40, 20, 10 };
             int[] a1 = { 5, 25, 50, 120 };
-            Console.WriteLine("[6, 4, 2, 1] ->");
-            pprint(climbingLeaderboard(s1, a1));
+            //Console.WriteLine("[6, 4, 2, 1] ->");
+            //pprint(climbingLeaderboard(s1, a1));
             
             int[] s2 = { 100, 90, 90, 80, 75, 60 };
             int[] a2 = { 50, 65, 77, 90, 102 };
-            Console.WriteLine("[6, 5, 4, 2, 1] ->");
-            pprint(climbingLeaderboard(s2, a2));
-            
+            //Console.WriteLine("[6, 5, 4, 2, 1] ->");
+            //pprint(climbingLeaderboard(s2, a2));
+
+            int[] s3 = { 100, 75, 50, 25 };
+            Console.WriteLine("[5, 4, 3, 1] ->");
+            pprint(climbingLeaderboard(s3, a1));
+
             Console.ReadKey();
         }
     }
