73dee89990ffe078129e41bb4a9d57d4cc2ac852
groiq <35578027+groiq@users.noreply.github.com>
2019-02-07 10:00:42 +0100

    set up exercise properly

--- hackerrank-com/04-climbing-the-leaderboard/04-climbing-the-leaderboard/Program.cs
+++ hackerrank-com/04-climbing-the-leaderboard/04-climbing-the-leaderboard/Program.cs
@@ -15,12 +15,10 @@ namespace hackerrank_com
             return result;
         }
 
-        static void arrOut(String test, int[] solution)
+        // quick way to print an array
+        static void pprint(int[] input)
         {
-            Console.Write(test + ": ");
-            //Console.WriteLine("[{0}]", string.Join(", ", yourArray));
-            Console.Write("[{0}]", string.Join(", ", solution));
-            Console.WriteLine();
+            Console.WriteLine("[{0}]", string.Join(", ", input));
         }
 
 
@@ -29,18 +27,14 @@ namespace hackerrank_com
             //Console.WriteLine("Output goes here...");
             int[] s1 = { 100, 100, 50, 40, 40, 20, 10 };
             int[] a1 = { 5, 25, 50, 120 };
-
-            arrOut("6,4,2,1", climbingLeaderboard(s1, a1));
-            //Console.WriteLine("6,4,2,1: " + climbingLeaderboard(s1, a1));
-
+            Console.WriteLine("[6, 4, 2, 1] ->");
+            pprint(climbingLeaderboard(s1, a1));
+            
             int[] s2 = { 100, 90, 90, 80, 75, 60 };
             int[] a2 = { 50, 65, 77, 90, 102 };
-
-            //Console.WriteLine("6,5,4,2,1: " + climbingLeaderboard(s2, a2));
-            arrOut("6,5,4,2,1", climbingLeaderboard(s2, a2));
-
-
-
+            Console.WriteLine("[6, 5, 4, 2, 1] ->");
+            pprint(climbingLeaderboard(s2, a2));
+            
             Console.ReadKey();
         }
     }
