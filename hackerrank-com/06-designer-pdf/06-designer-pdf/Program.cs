3d07143d315b4dc905b38b92a6b61208aea1cec1
groiq <35578027+groiq@users.noreply.github.com>
2019-02-08 11:13:30 +0100

    set up task in main method

--- hackerrank-com/06-designer-pdf/06-designer-pdf/Program.cs
+++ hackerrank-com/06-designer-pdf/06-designer-pdf/Program.cs
@@ -7,6 +7,13 @@ namespace hackerrank_com
     class Program
     {
 
+        static int designerPdfViewer(int[] h, string word)
+        {
+            int result = 0;
+
+            return result;
+        }
+
         // quick way to print an array
         static void pprint(int[] input)
         {
@@ -20,10 +27,27 @@ namespace hackerrank_com
 
         static void Main(string[] args)
         {
+            int[] i0 = { 1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 };
+            string w0 = "abc";
+            string o0 = "9";
+            Console.WriteLine(o0 + ": " + designerPdfViewer(i0, w0));
+            int[] i1 = { 1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7 };
+            string w1 = "zaba";
+            string o1 = "28";
+            Console.WriteLine(o1 + ": " + designerPdfViewer(i1, w1));
 
-            Console.WriteLine("Output goes here...");
 
             Console.ReadKey();
         }
     }
 }
+
+//arrays in main method created with copy-paste and this bit of python:
+//#!/usr/bin/env python3
+//input = ["1 3 1 3 1 4 1 3 2 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5",
+//         "1 3 1 3 1 4 1 3 2 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 7"]
+//for iStr in input:
+//    oStr = []
+//    for num in iStr.split():
+//        oStr.append(int(num))
+//    print(oStr)
