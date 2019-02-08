ebda98c2fa885b4f8f4ba136abe1b4faab38d8e8
groiq <35578027+groiq@users.noreply.github.com>
2019-02-08 10:57:20 +0100

    exercise designer pdf: set up project

---
+++ hackerrank-com/06-designer-pdf/06-designer-pdf/Program.cs
@@ -0,0 +1,29 @@
+﻿// workspace for exercises from https://www.hackerrank.com/
+
+using System;
+
+namespace hackerrank_com
+{
+    class Program
+    {
+
+        // quick way to print an array
+        static void pprint(int[] input)
+        {
+            Console.WriteLine("[{0}]", string.Join(", ", input));
+        }
+        // array toString function
+        static string arStr(int[] input)
+        {
+            return $"[{string.Join(", ", input)}]";
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Output goes here...");
+
+            Console.ReadKey();
+        }
+    }
+}
