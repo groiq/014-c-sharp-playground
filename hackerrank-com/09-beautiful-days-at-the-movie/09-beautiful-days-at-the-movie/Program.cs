6bec49a421dfdd4a25e4b5194b1779fc710e8186
groiq <35578027+groiq@users.noreply.github.com>
2019-02-09 15:59:44 +0100

    split number into array

--- hackerrank-com/09-beautiful-days-at-the-movie/09-beautiful-days-at-the-movie/Program.cs
+++ hackerrank-com/09-beautiful-days-at-the-movie/09-beautiful-days-at-the-movie/Program.cs
@@ -1,6 +1,7 @@
 ﻿// workspace for exercises from https://www.hackerrank.com/
 
 using System;
+using System.Collections.Generic;
 
 namespace hackerrank_com
 {
@@ -18,10 +19,40 @@ namespace hackerrank_com
             return $"[{string.Join(", ", input)}]";
         }
 
+        static int reverseNum(int input)
+        {
+            int result = 0;
+            var split = new List<int>();
+            int rest = input;
+            int digit;
+            int pos = 1;
+            while (rest > 0)
+            {
+                digit = rest % 10;
+                rest = rest / 10;
+                split.Add(digit);
+                pos *= 10;
+            }
+
+
+            int[] splitAr = new int[split.Count];
+            for (int i = 0; i < splitAr.Length; i++)
+            {
+                splitAr[i] = split[i];
+            }
+            Console.WriteLine(input + " - " + arStr(splitAr) +   " - " + result);
+            return result;
+        }
+
 
         static int beautifulDays(int i, int j, int k)
         {
             int result = 0;
+
+            for (int currNum = i; currNum < j; currNum++)
+            {
+                int currNumInv = reverseNum(currNum);
+            }
 
             return result;
         }
