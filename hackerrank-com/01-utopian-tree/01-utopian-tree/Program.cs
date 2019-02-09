6a0ab2c07cff43a97cd6ee5ed172ba2c49073247
groiq <35578027+groiq@users.noreply.github.com>
2019-02-09 12:59:47 +0100

    exercise: utopian tree - setup

---
+++ hackerrank-com/01-utopian-tree/01-utopian-tree/Program.cs
@@ -0,0 +1,40 @@
+﻿// workspace for exercises from https://www.hackerrank.com/
+
+using System;
+
+namespace hackerrank_com
+{
+    class Program
+    {
+
+        // quick way to print an array
+        static void pprint(int[] input)
+        {
+            Console.WriteLine("[{0}]", string.Join(", ", input));
+        }
+        // array toString function
+        static string arStr(int[] input)
+        {
+            return $"[{string.Join(", ", input)}]";
+        }
+
+
+        static int utopianTree(int n)
+        {
+
+            return n;
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i < 13; i++)
+            {
+                Console.WriteLine(i + " -> " + utopianTree(i));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
