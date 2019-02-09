be6725b3926978189b1840d9b29ef0fd306f8b04
groiq <35578027+groiq@users.noreply.github.com>
2019-02-09 15:42:41 +0100

    exercise beautiful days at the movie: setup

---
+++ hackerrank-com/09-beautiful-days-at-the-movie/09-beautiful-days-at-the-movie/Program.cs
@@ -0,0 +1,40 @@
+﻿// workspace for exercises from https://www.hackerrank.com/
+
+using System;
+
+namespace hackerrank_com
+{
+    class Program
+    {
+
+        // quick way to print an array
+        static void pprint(int[] input)
+        {
+            Console.WriteLine("[{0}]", string.Join(", ", input));
+        }
+        // array toString function
+        static string arStr(int[] input)
+        {
+            return $"[{string.Join(", ", input)}]";
+        }
+
+
+        static int beautifulDays(int i, int j, int k)
+        {
+            int result = 0;
+
+            return result;
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(beautifulDays(5, 15, 2));
+            Console.WriteLine(beautifulDays(95, 105, 2));
+
+
+            Console.ReadKey();
+        }
+    }
+}
