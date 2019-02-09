950be13cc5bc7b70ffbbbd274c4eeb319015aa87
groiq <35578027+groiq@users.noreply.github.com>
2019-02-09 08:49:06 +0100

    new exercise: calculate the difference between two dates

---
+++ dateDifferences/dateDifferences/Program.cs
@@ -0,0 +1,33 @@
+﻿// calculate differences between two dates.
+
+using System;
+
+namespace hackerrank_com
+{
+    class Program
+    {
+
+        // quick way to print an array
+        static void pprint(int[] input)
+        {
+            Console.WriteLine("[{0}]", string.Join(", ", input));
+        }
+        // array to string function
+        static string arStr(int[] input)
+        {
+            return $"[{string.Join(", ", input)}]";
+        }
+
+
+        // design function here
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Output goes here...");
+
+            Console.ReadKey();
+        }
+    }
+}
