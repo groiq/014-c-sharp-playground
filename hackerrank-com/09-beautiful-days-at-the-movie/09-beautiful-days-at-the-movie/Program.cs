b401bcca89f601b018ce19a7c4849b03a0904ca4
groiq <35578027+groiq@users.noreply.github.com>
2019-02-09 16:13:03 +0100

    exercise beautiful days at the movies: done

--- hackerrank-com/09-beautiful-days-at-the-movie/09-beautiful-days-at-the-movie/Program.cs
+++ hackerrank-com/09-beautiful-days-at-the-movie/09-beautiful-days-at-the-movie/Program.cs
@@ -32,6 +32,13 @@ namespace hackerrank_com
                 rest = rest / 10;
                 split.Add(digit);
                 pos *= 10;
+                
+            }
+            pos /= 10;
+            foreach (var item in split)
+            {
+                result += (item * pos);
+                pos /= 10;
             }
 
 
@@ -40,18 +47,22 @@ namespace hackerrank_com
             {
                 splitAr[i] = split[i];
             }
-            Console.WriteLine(input + " - " + arStr(splitAr) +   " - " + result);
+            //Console.WriteLine(input + " - " + arStr(splitAr) +   " - " + result);
             return result;
         }
 
 
         static int beautifulDays(int i, int j, int k)
         {
+            Console.WriteLine(i + " - " + j + " - " + k);
             int result = 0;
 
-            for (int currNum = i; currNum < j; currNum++)
+            for (int currNum = i; currNum <= j; currNum++)
             {
                 int currNumInv = reverseNum(currNum);
+                int diff = Math.Abs(currNum - currNumInv);
+                if (diff % k == 0) { result += 1; }
+                Console.WriteLine(currNum + " - " + currNumInv + " - " + diff + " - " + (diff % k));
             }
 
             return result;
@@ -61,8 +72,9 @@ namespace hackerrank_com
         static void Main(string[] args)
         {
 
-            Console.WriteLine(beautifulDays(5, 15, 2));
-            Console.WriteLine(beautifulDays(95, 105, 2));
+            //Console.WriteLine(beautifulDays(5, 15, 2));
+            //Console.WriteLine(beautifulDays(95, 105, 2));
+            Console.WriteLine(beautifulDays(13, 45, 3));
 
 
             Console.ReadKey();
