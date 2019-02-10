6c882538588f1ac879848b69c5006ac3a3c2f888
groiq <35578027+groiq@users.noreply.github.com>
2019-02-10 16:06:04 +0100

    exercise permutation equation: setup

---
+++ hackerrank-com/13-permutation-equation/13-permutation-equation/Program.cs
@@ -0,0 +1,43 @@
+﻿// workspace for exercises from https://www.hackerrank.com/
+
+using System;
+
+namespace hackerrank_com
+{
+    class Program
+    {
+
+        // quick way to print an array
+        static void pprint(int[] input)
+        {
+            Console.WriteLine("[{0}]", string.Join(", ", input));
+        }
+        // array toString function
+        static string arStr(int[] input)
+        {
+            return $"[{string.Join(", ", input)}]";
+        }
+
+
+        // design function here
+        static int[] permutationEquation(int[] p)
+        {
+            int[] result = new int[p.Length];
+
+            return result;
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            int[] i0 = { 2, 3, 1 };
+            Console.WriteLine(arStr(i0) + " -> {2,3,1} -> " + arStr(permutationEquation(i0)));
+            int[] i1 = { 4, 3, 5, 1, 2 };
+            Console.WriteLine(arStr(i1) + " -> {1,3,5,4,2} -> " + arStr(permutationEquation(i1)));
+            
+
+            Console.ReadKey();
+        }
+    }
+}
