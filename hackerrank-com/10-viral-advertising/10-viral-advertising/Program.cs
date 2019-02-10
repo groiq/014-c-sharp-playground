217eb38b2834a3dfd9a8a1ed7c85f91bbe3240b4
groiq <35578027+groiq@users.noreply.github.com>
2019-02-10 08:57:17 +0100

    exercise viral advertising: done

--- hackerrank-com/10-viral-advertising/10-viral-advertising/Program.cs
+++ hackerrank-com/10-viral-advertising/10-viral-advertising/Program.cs
@@ -21,9 +21,17 @@ namespace hackerrank_com
 
         static int viralAdvertising(int n)
         {
+            int shared = 5;
+            int liked = shared / 2;
+            int cumulative = liked;
+            for (int i = 1; i < n; i++)
+            {
+                shared = liked * 3;
+                liked = shared / 2;
+                cumulative += liked;
+            }
 
-
-            return n;
+            return cumulative;
         }
 
 
