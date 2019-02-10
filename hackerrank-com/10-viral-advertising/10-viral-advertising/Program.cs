5ede92d9624244c23158b42c4fcbf96e30de5f52
groiq <35578027+groiq@users.noreply.github.com>
2019-02-10 08:40:48 +0100

    exercise viral marketing: setup

---
+++ hackerrank-com/10-viral-advertising/10-viral-advertising/Program.cs
@@ -0,0 +1,38 @@
+﻿// workspace for exercises from https://www.hackerrank.com/
+
+using System;
+
+namespace hackerrank_com
+{
+    class Program
+    {
+
+        // quick way to print an array
+        static void pprint(int[] input)
+        {
+            Console.WriteLine("[{0}]", string.Join(", ", input));
+        }
+        // array toString function
+        static string arStr(int[] input)
+        {
+            return $"[{string.Join(", ", input)}]";
+        }
+
+
+        static int viralAdvertising(int n)
+        {
+
+
+            return n;
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Output goes here...");
+
+            Console.ReadKey();
+        }
+    }
+}
