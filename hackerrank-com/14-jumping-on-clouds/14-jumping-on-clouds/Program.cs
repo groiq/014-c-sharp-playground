15f691f3cea9cf0419abd0a278de66cf0d676ca4
groiq <35578027+groiq@users.noreply.github.com>
2019-02-13 20:03:45 +0100

    exercise jumping on clouds: done

--- hackerrank-com/14-jumping-on-clouds/14-jumping-on-clouds/Program.cs
+++ hackerrank-com/14-jumping-on-clouds/14-jumping-on-clouds/Program.cs
@@ -23,6 +23,14 @@ namespace hackerrank_com
         static int jumpingOnClouds(int[] c, int k)
         {
             int energy = 100;
+            int pos = 0;
+            do
+            {
+                pos = (pos + k) % c.Length;
+                energy -= 1;
+                if (c[pos] == 1) { energy -= 2; }
+                //Console.WriteLine("pos: " + pos + ", energy: " + energy);
+            } while (pos != 0);
 
             return energy;
         }
@@ -31,18 +39,25 @@ namespace hackerrank_com
         static void Main(string[] args)
         {
 
-            /*
-             * Sample Input
-
-8 2
-0 0 1 0 0 1 1 0
-
-Sample Output
-
-92
-*/
             int[] i0 = { 0, 0, 1, 0, 0, 1, 1, 0 };
             Console.WriteLine("92: " + jumpingOnClouds(i0,2));
+            int[] i1 = { 1, 1, 1, 0, 1, 1, 0, 0, 0, 0 };
+            Console.WriteLine("94: " + jumpingOnClouds(i1,3));
+/*            Input(stdin)
+Download
+
+10 3
+1 1 1 0 1 1 0 0 0 0
+
+Your Output(stdout)
+
+80
+
+Expected Output
+Download
+
+94*/
+
 
             Console.ReadKey();
         }
